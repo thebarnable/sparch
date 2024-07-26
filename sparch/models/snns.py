1670d67431fdeb5e82d51ab027a3301a3d881910
Tim Stadtmann <timstadtmann@gmail.com>
2024-07-26 10:35:02 +0200

    implement one-spike-only scheme for balanced network

--- sparch/models/snns.py
+++ sparch/models/snns.py
@@ -18,18 +18,42 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from scipy.signal import butter, filtfilt
 
-
 class SpikeFunctionBoxcar(torch.autograd.Function):
     """
     Compute surrogate gradient of the spike step function using
     box-car function similar to DECOLLE, Kaiser et al. (2020).
     """
-
     @staticmethod
     def forward(ctx, x):
         ctx.save_for_backward(x)
+
         return x.gt(0).float()
 
+    @staticmethod
+    def backward(ctx, grad_spikes):
+        (x,) = ctx.saved_tensors
+        grad_x = grad_spikes.clone()
+        grad_x[x <= -0.5] = 0
+        grad_x[x > 0.5] = 0
+        return grad_x
+
+
+class SingleSpikeFunctionBoxcar(torch.autograd.Function):
+    """
+    Compute surrogate gradient of the spike step function using
+    box-car function similar to DECOLLE, Kaiser et al. (2020), but allowing only spike to happen in forward().
+    """
+    @staticmethod
+    def forward(ctx, x):
+        ctx.save_for_backward(x)
+
+        x_copy = x.clone()
+        x[:, :] = 0
+        x[torch.arange(x.shape[0]), torch.argmax(x_copy, dim=1)] = 1
+        x[x_copy<=0] = 0
+        return x
+
+    @staticmethod
     def backward(ctx, grad_spikes):
         (x,) = ctx.saved_tensors
         grad_x = grad_spikes.clone()
@@ -577,8 +601,8 @@ class RLIFLayer(nn.Module):
         self.bidirectional = bidirectional
         self.batch_size = self.batch_size * (1 + self.bidirectional)
         self.alpha_lim = [np.exp(-1 / 5), np.exp(-1 / 25)]
-        self.spike_fct = SpikeFunctionBoxcar.apply
         self.balance = balance
+        self.spike_fct = SpikeFunctionBoxcar.apply if balance is False else SingleSpikeFunctionBoxcar.apply
 
         # Trainable parameters
         self.W = nn.Linear(self.input_size, self.hidden_size, bias=use_bias)
