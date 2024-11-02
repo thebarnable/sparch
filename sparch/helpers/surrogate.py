ae729c6ac365f18fc349e3a19b561466263a2fe0
Tim Stadtmann <timstadtmann@gmail.com>
2024-11-02 11:05:34 +0100

    now multiple batches will each have one spike if --single-spike

--- sparch/helpers/surrogate.py
+++ sparch/helpers/surrogate.py
@@ -58,8 +58,24 @@ class SingleSpikeFunctionBoxcar(torch.autograd.Function):
         x[:, :] = 0
         spike_ids = torch.nonzero(x_copy > v_thresh, as_tuple=False)
         if len(spike_ids) > 0:
-            spike_id = spike_ids[torch.randint(len(spike_ids), (1,))][0] # choose random neuron to spike
-            x[spike_id[0]][spike_id[1]] = 1
+            #spike_id = spike_ids[torch.randint(len(spike_ids), (1,))][0] # choose random neuron to spike
+            # Get unique values in the first column and their inverse indices
+            unique_x, inverse_indices = torch.unique(spike_ids[:, 0], return_inverse=True)
+
+            # Randomly shuffle each duplicate set and pick the first occurrence
+            selected_indices = []
+            for i in range(len(unique_x)):
+                # Get all indices of current unique x value
+                indices = (inverse_indices == i).nonzero(as_tuple=True)[0]
+                # Choose one random index from these
+                random_index = indices[torch.randint(len(indices), (1,))]
+                selected_indices.append(random_index)
+
+            # Construct the result tensor
+            indices = spike_ids[torch.tensor(selected_indices).squeeze()]
+            if len(indices.shape) == 1:
+                indices = indices.unsqueeze(dim=0)
+            x[indices[:, 0],indices[:, 1]] = 1
         return x
 
     @staticmethod
