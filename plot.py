8fe12e44f8f640db3940e2324932dbe82524f7af
Tim Stadtmann <timstadtmann@gmail.com>
2024-11-05 09:22:57 +0100

    plot.py now recurses through given dir and automatically plots all data

--- plot.py
+++ plot.py
@@ -24,7 +24,6 @@ DATA_FOLDER_REMOTE="/mnt/data4tb2/stadtmann/paper/beep"
 DATA_FOLDER_HOME="/home/stadtmann/1_Projects/sparch"
 DATA_FOLDER_HO="/home/tim/Projects/beep"
 OUTPUT_FOLDER=DATA_FOLDER_HOME+"/plots"
-Path(OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
 
 SAVE=True
 PLOT=False
@@ -46,50 +45,65 @@ def params_from_file_name(filename):
 
     f=filename.split("/")[-1].split("_")
     return f[IDX_DATASET], f[IDX_NEURON], f[IDX_LAYER].split("lay")[0], f[IDX_LAYER].split("lay")[1], float(f[IDX_DROPOUT][-1]+"."+f[IDX_DROPOUT+1]), f[IDX_NORM], int(f[IDX_ST].split("st")[1])
-    
-def plot_results():
-    # results: ['train_accs', 'train_frs', 'validation_accs', 'validation_frs', 'test_acc', 'test_fr', 'best_acc', 'best_epoch']
 
-    # folders = [os.path.join(DATA_FOLDER_HOME+"/results/dvs_beep", file) for file in os.listdir(DATA_FOLDER_HOME+"/results/dvs_beep")]
-    # folders += [os.path.join(DATA_FOLDER_HOME+"/results/dvs_eprop", file) for file in os.listdir(DATA_FOLDER_HOME+"/results/dvs_eprop")]
-    # folders += [DATA_FOLDER_HOME+"/results/optuna_dvs_eprop_HPO/id=356756794"]
-    #folders = [os.path.join(DATA_FOLDER_HO+"/results/dvs_eprop", file) for file in os.listdir(DATA_FOLDER_HO+"/results/dvs_beep")]
-    #folders = [DATA_FOLDER_HO+"/exp"]
-    folders = [os.path.join(DATA_FOLDER_HOME+"/exp", file) for file in os.listdir(DATA_FOLDER_HOME+"/exp")]
+def recurse_dir(path):
+    folders = []
+    for (dirpath, dirs, files) in os.walk(path):
+        # potential result folder if 
+        # - contains run0 or trial_0 folders
+        # - contains result.pth directly
+        if "run0" in dirs or "trial_0" in dirs or ("results.pth" in files and "run" not in dirpath and "trial_" not in dirpath):
+            folders.append(dirpath)
+
+    return folders
+
+def plot_results(path):
+    path = os.path.abspath(path)
+    folders = recurse_dir(path)
+    fig, axs = plt.subplots(3, 1, sharex=True, gridspec_kw={'height_ratios': [1, 1, 1]}, figsize=(30,15))
+    fig.subplots_adjust(hspace=0)
+
     colors  = [BLUE,RED,GREEN,YELLOW,VIOLET, DARKRED, DARKBLUE, GREY]
-
     for i,folder in enumerate(folders):
-        #print(f"Analyzing {folder}... ")
-
-        if not os.path.isdir(folder):
-            print("Skipping",folder,"because not a directory")
-            continue
-        
         if not args.new_name:
             dataset, neuron, n_layers, n_neurons, dropout, norm, st = params_from_file_name(folder)
             print(f"Results for {n_layers} layers, {n_neurons} {neuron} neurons on {dataset} (dropout = {dropout}, norm = {norm}, {st}x repeated inputs)")
         else:
             print(f"Results for {folder}")
 
-        validation_accs = []
-        test_accs = []
-        for trial_folder in os.walk(folder):
-            if "results.pth" in trial_folder[2]:
-                validation_accs.append(torch.tensor(torch.load(trial_folder[0]+"/results.pth")["validation_accs"]))
-                test_accs.append(torch.load(trial_folder[0]+"/results.pth")['test_acc'])
-            
-        if len(validation_accs) == 0:
+        validation_results, test_results = {"acc": [], "fr": [], "balance": []}, {"acc": [], "fr": [], "balance": []}
+        skip=False
+        for j,metric in enumerate(["acc", "fr", "balance"]):
+            for trial_folder in os.walk(folder):
+                if "results.pth" in trial_folder[2]:
+                    validation_data = torch.load(trial_folder[0]+"/results.pth")["validation_"+metric+"s"]
+                    test_data = torch.load(trial_folder[0]+"/results.pth")["test_"+metric]
+                    if metric=="fr":
+                        validation_data=np.array(validation_data).tolist()
+                        #test_data=test_data
+
+                    validation_results[metric].append(torch.tensor(validation_data))
+                    test_results[metric].append(test_data)
+                
+            if len(validation_results["acc"]) == 0:
+                skip=True
+                continue
+
+            validation_results[metric] = torch.stack(validation_results[metric])
+            x = list(range(0,validation_results[metric].shape[1]))
+            y_mean = validation_results[metric].mean(axis=0)
+            y_ci = validation_results[metric].std(axis=0) #1.96 * np.std(results["score"], axis=0)/np.sqrt(len(x))
+            axs[j].plot(x, y_mean, color=colors[i%8], label=folder)
+            #plt.plot(x, results["train_score"].mean(axis=0), color=colors[i%8], alpha=.1)
+            axs[j].fill_between(x, (y_mean-y_ci), (y_mean+y_ci), color=colors[i%8], alpha=.1)
+            axs[j].set_ylabel(metric)
+            #axs[j].set_title(metric, y=0.5)
+
+        if skip:
             print("Skipping",folder,"because it contains no results!")
             continue
 
-        validation_accs = torch.stack(validation_accs)
-        x = list(range(0,validation_accs.shape[1]))
-        y_mean = validation_accs.mean(axis=0)
-        y_ci = validation_accs.std(axis=0) #1.96 * np.std(results["score"], axis=0)/np.sqrt(len(x))
-        plt.plot(x, y_mean, color=colors[i%8], label=folder)
-        #plt.plot(x, results["train_score"].mean(axis=0), color=colors[i%8], alpha=.1)
-        plt.fill_between(x, (y_mean-y_ci), (y_mean+y_ci), color=colors[i%8], alpha=.1)
-
+        validation_accs, test_accs = validation_results["acc"], test_results["acc"]
         print(f"(1) Highest validation accuracy (total): {validation_accs.max()*100:.2f}%")
         print(f"(2) Highest validation accuracy (avg over trial): {validation_accs.mean(axis=0).max()*100:.2f}%")
         print(f"(3) Average validation accuracy over last 5 epochs (avg over trial): {validation_accs.mean(axis=0)[-5:].mean()*100:.2f}%")
@@ -97,9 +111,10 @@ def plot_results():
         print("")
 
     plt.xlabel('Epoch')
-    plt.ylabel('Validation Accuracies')
+    #plt.ylabel('Validation Accuracies')
     plt.legend()
     if SAVE:
+        Path(OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
         plt.savefig(OUTPUT_FOLDER+"/validation_accs.png")
     if PLOT:
         plt.show()
@@ -108,6 +123,7 @@ def plot_results():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Plot script')
+    parser.add_argument('path', type=str, help='Either a folder containing a results.pth or a parent folder of one')
     parser.add_argument('--plot', action='store_true', help='Show plots')
     parser.add_argument('--save', action='store_true', help='Save plots')
     parser.add_argument('--score', default='validation_accs', help='Values to plot on x-axis (only used in some functions; can be anything from results.pth)')
@@ -122,4 +138,4 @@ if __name__ == '__main__':
     if args.function != "":
         locals()[args.function]()
     else:
-        plot_results()
+        plot_results(args.path)
